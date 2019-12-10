8d5068edf86b07acf72ef8c6c52bd6d5ecc149db
Slava Bezgachev <bezgachev@gmail.com>
2019-12-10 15:42:03 +0600

    Components and pages done

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -10,7 +10,14 @@ import PropTypes from "prop-types";
 import Helmet from "react-helmet";
 import { useStaticQuery, graphql } from "gatsby";
 
-function SEO({ description, lang, meta, title }) {
+interface Props {
+  description?: string;
+  lang: string;
+  meta?: [];
+  title: string;
+}
+
+function SEO({ description, lang, meta, title }: Props) {
   const { site } = useStaticQuery(
     graphql`
       query {
@@ -67,7 +74,7 @@ function SEO({ description, lang, meta, title }) {
           name: `twitter:description`,
           content: metaDescription
         }
-      ].concat(meta)}
+      ].concat(meta || [])}
     />
   );
 }
